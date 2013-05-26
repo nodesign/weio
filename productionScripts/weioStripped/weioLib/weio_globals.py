dd980d59fdaf760be3725729419eb624677d2a4e
Uros Petrevski <uros@nodesign.net>
2013-05-26 23:23:17 +0200

    connected to wifi bug corrected

---
+++ productionScripts/weioStripped/weioLib/weio_globals.py
@@ -0,0 +1,43 @@
+### 
+#
+# WEIO Web Of Things Platform
+# Copyright (C) 2013 Nodesign.net, Uros PETREVSKI, Drasko DRASKOVIC
+# All rights reserved
+#
+#               ##      ## ######## ####  #######  
+#               ##  ##  ## ##        ##  ##     ## 
+#               ##  ##  ## ##        ##  ##     ## 
+#               ##  ##  ## ######    ##  ##     ## 
+#               ##  ##  ## ##        ##  ##     ## 
+#               ##  ##  ## ##        ##  ##     ## 
+#                ###  ###  ######## ####  #######
+#
+#                    Web Of Things Platform 
+#
+# This file is part of WEIO
+# WEIO is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# WEIO is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors : 
+# Uros PETREVSKI <uros@nodesign.net>
+# Drasko DRASKOVIC <drasko.draskovic@gmail.com>
+#
+###
+
+HIGH = '1'
+LOW = '0'
+
+INPUT = 'in'
+OUTPUT = 'out'
+
+
