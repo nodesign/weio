bbd099de02eea044e659078f9734989b818d1422
Drasko DRASKOVIC <drasko.draskovic@devialet.com>
2013-06-14 22:17:58 +0200

    corrections

--- openWrt/pkg_install.sh
+++ openWrt/pkg_install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# Update package base
+./scripts/feeds update
+
+# Install base packages
+./scripts/feeds install
+
+# Install additional packages
 ./scripts/feeds install python
 ./scripts/feeds install avahi-daemon
 ./scripts/feeds install ntpd
