a9775769a9f82bf9c7cf6a552cd571061a848148
Uros Petrevski <uros@nodesign.net>
2013-07-16 23:16:40 +0200

    diskspace usage and free space code

---
+++ sandbox/diskSpace/diskSpace.py
@@ -0,0 +1,18 @@
+import os
+from collections import namedtuple
+
+_ntuple_diskusage = namedtuple('usage', 'total used free')
+
+def disk_usage(path):
+    """Return disk usage statistics about the given path.
+
+    Returned valus is a named tuple with attributes 'total', 'used' and
+    'free', which are the amount of total, used and free space, in Megabytes.
+    """
+    st = os.statvfs(path)
+    free = st.f_bavail * st.f_frsize
+    total = st.f_blocks * st.f_frsize
+    used = (st.f_blocks - st.f_bfree) * st.f_frsize
+    return _ntuple_diskusage(total/1000000, used/1000000, free/1000000)
+    
+print disk_usage('/').free
