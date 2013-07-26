b52864e0f872067d97c679522e4e0262501e645c
Angelo Chiacchio <info@areox.net>
2013-07-26 22:53:28 +0200

    Correction dropdown at padding sidebar

--- www/libs/weio/editor.js
+++ www/libs/weio/editor.js
@@ -90,6 +90,7 @@ $(document).ready(function () {
                 
    initEditor();
    updateConsoleHeight();
+ 
    
    //console.log(window.innerHeight); 
    //console.log(editorData.editors.length);
