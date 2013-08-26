b31edcdd7f84e05f699cc5d008209ac2a7c7bb2a
Drasko DRASKOVIC <drasko.draskovic@gmail.com>
2013-08-27 00:53:17 +0200

    Add Tornado and SockJS Python packages. They will now be automatically present in sysimage.

--- openWrt/installWeio.sh
+++ openWrt/installWeio.sh
@@ -12,39 +12,44 @@
 #   - Copy needed files from it
 #   - Install Carambola2 OpenWRT packages needed for WeIO
 #   - Copy appropriate configuration for WeIO
+#
+# Usage:    Either supply weio dir as the first argument,
+#           or it will be cloned from GitHub
 ###
 
 
 BUILD_DIR=$PWD
 
+if [ $# -eq 0 ]
+  then
+    # No local WeIO dir supplied, get WeIO from GitHub
+    git clone https://github.com/nodesign/weio.git
+    WEIO=weio
+else
+    WEIO=$1
+fi
+
 # Clean Carambola2 SDK
 make clean distclean
 
-# Get WeIO
-git clone https://github.com/nodesign/weio.git
-
 # Copy WeIO image files
-cp -r weio/openWrt/files .
+cp -r $WEIO/openWrt/files .
 
 # Get the newest WeIO verion into the `files`
-cd weio/productionScripts/
+cd $WEIO/productionScripts/
 ./stripMe.sh
 cd $BUILD_DIR
-tar -xzvf weio/productionScripts/weio.tar.gz -C files/
-
-# Get installed Python libraries for Carambola and extract them to files (/usr directory)
-wget http://www.we-io.net/downloads/weioImage/weioLibs.tar.gz
-tar -xzvf weioLibs.tar.gz -C files/
-rm weioLibs.tar.gz
+tar -xzvf $WEIO/productionScripts/weio.tar.gz -C files/
 
 # Install needed packages for WeIO
-cp weio/openWrt/pkg_install.sh .
+cp $WEIO/openWrt/pkg_install.sh .
 ./pkg_install.sh
 
 # Copy configuration
-cp weio/openWrt/weio-config-carambola2 .config
+cp $WEIO/openWrt/weio-config-carambola2 .config
 
 # Build
+echo ""
 echo "Now you can build the image by running: make -j 4"
 
 
