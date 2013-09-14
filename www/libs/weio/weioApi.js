3057a2a1cda1197aa10b41eba19791578c5fac59
Uros Petrevski <uros@nodesign.net>
2013-09-14 10:53:50 +0200

    Avoid sending messages when websocket is not fully opened

--- www/libs/weio/weioApi.js
+++ www/libs/weio/weioApi.js
@@ -35,29 +35,57 @@
 *
 **/
 
-
+/*
+ * WeIO websocket object
+ */
 var _weio;
-var addr = location.host;
-if (addr.indexOf(":")!=-1) {
-    var a = addr.split(":");
-    addr = 'http://' + a[0] + ':8082/api';
-} else {
-    var a = 'http://' + addr + ':8082/api';
-    addr = a;
-}
-console.log(addr);
-_weio = new SockJS(addr);
 
+/*
+ * Identify server address and port to open websocket
+ */
+var _addr = location.host;
+if (_addr.indexOf(":")!=-1) {
+    var a = _addr.split(":");
+    _addr = 'http://' + a[0] + ':8082/api';
+} else {
+    var a = 'http://' + _addr + ':8082/api';
+    _addr = a;
+}
+console.log("WebSocket connecting to " + _addr);
+
+/*
+ * WebSocket openning
+ */
+_weio = new SockJS(_addr);
+
+
+/* 
+ * GLOBALS
+ */
 var HIGH = "1";
 var LOW = "0";
 var OUTPUT = "out";
 var INPUT = "in";
 
-_weio.onopen = function() {
-  console.log('socket opened for weio API');
-};
+/*
+ * Unique UUID number of this session
+ * Will be generated on oppening of websocket and will be sent with
+ * every message to server 
+ */
+var uuid;
 
-
+/* 
+ * Generate unique UUID number, uuid4 conforms to standard
+ */
+function _generateUUID(){
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x7|0x8)).toString(16);
+    });
+    return uuid;
+}
 
 
 
@@ -66,7 +94,31 @@ _weio.onopen = function() {
  * Define callbacks here and request keys
  * Each key is binded to coresponding function
  */
-var callbacks = {}
+var weioCallbacks = {}
+
+
+_weio.onopen = function() {
+    console.log('socket opened for weio API');
+    
+    // Client info will be sent to server
+    data = {};
+    data.appCodeName = navigator.appCodeName;
+    data.appName = navigator.appName;
+    data.appVersion = navigator.appVersion;
+    data.cookieEnabled = navigator.cookieEnabled;
+    data.onLine = navigator.onLine;
+    data.platform = navigator.platform;
+    data.userAgent = navigator.userAgent;
+    
+    uuid = _generateUUID()
+    data.uuid = uuid;
+    
+    // Say hello to server with some client data
+    var rq = { "request": "_info", "data":data};
+    _weio.send(JSON.stringify(rq));
+
+};
+
 
 _weio.onmessage = function(e) {
     // JSON data is parsed into object
@@ -76,14 +128,14 @@ _weio.onmessage = function(e) {
     if ("requested" in data) {
         // this is instruction that was echoed from server + data as response
         instruction = data.requested;  
-        if (instruction in callbacks) 
-            callbacks[instruction](data);
+        if (instruction in weioCallbacks) 
+            weioCallbacks[instruction](data);
     } else if ("serverPush" in data) {
         // this is instruction that was echoed from server + data as response
         
         instruction = data.serverPush;  
-        if (instruction in callbacks) 
-            callbacks[instruction](data);
+        if (instruction in weioCallbacks) 
+            weioCallbacks[instruction](data);
         
     }
 
@@ -93,6 +145,9 @@ _weio.onclose = function() {
   console.log('socket is closed for editor');
 };
 
+/* 
+ * Low level electronics instructions from JS
+ */
 function digitalWrite(pin, value) {
   genericMessage("digitalWrite", [pin,value]);
 };
@@ -101,8 +156,19 @@ function pinMode(pin, mode) {
   genericMessage("pinMode", [pin,mode]);
 };
 
+
+
+
+/*
+ * Generic handler for sending messages to server
+ */
 function genericMessage(instruction, data) {
-    var askWeio = { "request": instruction, "data" : data };
-    _weio.send(JSON.stringify(askWeio));
+    // avoid sending messages to websocket that is not yet opened
+    if (_weio.readyState != 0) {
+        var askWeio = { "request": instruction, "data" : data, "uuid" : uuid };
+        _weio.send(JSON.stringify(askWeio));
+    } else {
+        console.log("Warning, message tried to be send when websocket was not yet completely opened");
+    }
 }
 
