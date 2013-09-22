dc24038527cb4e1f8787e94093345b3d8bf4861e
Drasko DRASKOVIC <drasko.draskovic@gmail.com>
2013-09-23 00:10:09 +0200

    Added thread-safe proxy class and print (to both stdout and file)

--- weioLib/weioUserApi.py
+++ weioLib/weioUserApi.py
@@ -1,9 +1,61 @@
+from __future__ import print_function
+from threading import Lock, RLock
+import sys
+
 # Global definitions
 ONCE = 0
 ALWAYS = 1
 
 OUTPUT = 0
 INPUT = 1
+
+
+#####################
+# WeIO User Classes
+#####################
+###
+# LockProxy is used to lock methods of inherited class
+# Example of usage:
+# lockedset = LockProxy(set([1,2,3]))
+###
+class LockProxy(object):
+    def __init__(self, obj):
+        self.__obj = obj
+        self.__lock = RLock()
+        # RLock because object methods may call own methods
+    def __getattr__(self, name):
+        def wrapped(*a, **k):
+            with self.__lock:
+                getattr(self.__obj, name)(*a, **k)
+        return wrapped
+
+###
+# Locking can be done with class decorators
+###
+def lockedMethod(method):
+    """Method decorator. Requires a lock object at self._lock"""
+    def newmethod(self, *args, **kwargs):
+        with self._lock:
+            return method(self, *args, **kwargs)
+    return newmethod
+
+###
+# Thread-safe (locaked) print
+###
+class WeioPrint():
+    def __init__(self, *args, **kwargs):
+        self._lock = Lock()
+    
+    @lockedMethod
+    def output(self, *args, **kwargs):
+        print(*args, **kwargs)
+
+        # Flush the file or stdout
+        if (kwargs.get('file') != None):
+            kwargs['file'].flush()
+        else:
+            sys.stdout.flush()
+
 
 class WeioSharedVar(object):
     pass
@@ -48,10 +100,11 @@ class WeioClient():
     def __init__(self, info, connection):
         self.info = info
         self.connection = connection
-        
+
 
 ###
 # Global instances
 ###
 attach = WeioAttach()
 shared = WeioSharedVar()
+console = WeioPrint()
