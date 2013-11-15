12dd26fedc15e0086c48fa778a0720307ee6cda0
Uros Petrevski <uros@nodesign.net>
2013-11-15 10:49:15 +0100

    Oscillo example updated

--- userProjects/webExample2/main.py
+++ userProjects/webExample2/main.py
@@ -1,55 +1,72 @@
-from weioLib.weioUserApi import shared
-from weioLib.weioIO import *
-from weioLib import weio
-import colorsys
 
-def setup():
-    setPwmPeriod(2000)
+from weioLib.weioUserApi import shared, attach
+from weioLib.weioIO import pwmWrite, analogRead
+
+import colorsys
+import time
 
 def buttonHandler(dataIn) :
-    if dataIn is not None :
-        print dataIn
-        #print "FROM BROWSER: ", dataIn["data"], " uuid: ", dataIn["uuid"]
-        beta = float(dataIn)
-        
-        # put limiters
-        if (beta>88):
-            beta = 88
-        if (beta<-88):
-            beta = -88
-        
-        # map interval between -88 and 88 to 0.0 - 1.0
-        hue = proportion(beta,-88,88, 0,1)
-        # drive HUE color and transform to rgb for LED 
-        rgb = colorsys.hsv_to_rgb(hue, 1.0, 1.0)
-        
-        # rgb output is 0.0-1.0 transform to 0-255
-        red = int(rgb[0]*255.0)
-        green = int(rgb[1]*255.0)
-        blue = int(rgb[2]*255.0)
-        
-        colorData = {}
-        colorData["red"] = red
-        colorData["green"] = green
-        colorData["blue"] = blue
-        
-        #print red, " ", green, " ", blue
-        
-        # Export color to LED
-        setColor(red,green,blue)
-        
-        for client in weio.shared.connectedClients :
-            client.connection.emit("usrMsg", colorData)
+   if dataIn is not None :
+       print dataIn
+       #print "FROM BROWSER: ", dataIn["data"], " uuid: ", dataIn["uuid"]
+       beta = float(dataIn)
 
-      
+       # put limiters
+       if (beta>88):
+           beta = 88
+       if (beta<-88):
+           beta = -88
+
+       # map interval between -88 and 88 to 0.0 - 1.0
+       hue = proportion(beta,-88.0,88.0, 0.0,1.0)
+       # drive HUE color and transform to rgb for LED 
+       rgb = colorsys.hsv_to_rgb(hue, 1.0, 1.0)
+
+       # rgb output is 0.0-1.0 transform to 0-255
+       red = int(rgb[0]*255.0)
+       green = int(rgb[1]*255.0)
+       blue = int(rgb[2]*255.0)
+
+       # Export color to LED
+       setColor(red,green,blue)
+
 def setColor(r,g,b):
-    pwmWrite(19,255-b)
-    pwmWrite(20,255-r)
-    pwmWrite(21,255-g)
+   pwmWrite(19,255-b)
+   pwmWrite(20,255-r)
+   pwmWrite(21,255-g)
 
+   colorData = {}
+   colorData["red"] = r
+   colorData["green"] = g
+   colorData["blue"] = b
+
+   for client in shared.connectedClients :
+           client.connection.emit("usrMsg", colorData)
+
+def potar() :
+   val = 0
+   val_p = 0
+   while True:
+       val = analogRead(25)
+
+       if (abs(val-val_p)>5):
+           hue = proportion(val,0,1023, 0.0,1.0)
+           # drive HUE color and transform to rgb for LED 
+           rgb = colorsys.hsv_to_rgb(hue, 1.0, 1.0)
+
+           # rgb output is 0.0-1.0 transform to 0-255
+           red = int(rgb[0]*255.0)
+           green = int(rgb[1]*255.0)
+           blue = int(rgb[2]*255.0)
+
+           setColor(red,green,blue)
+
+       val_p = val
+       time.sleep(0.1)
 
 def proportion(value,istart,istop,ostart,ostop) :
-        return float(ostart) + (float(ostop) - float(ostart)) * ((float(value) - float(istart)) / (float(istop) - float(istart)))
+       return float(ostart) + (float(ostop) - float(ostart)) * ((float(value) - float(istart)) / (float(istop) - float(istart)))
 
 # Attaches interrupt from Web client to "message" string
-weio.attach.event('message', buttonHandler)
+attach.event('message', buttonHandler)
+attach.process(potar)
