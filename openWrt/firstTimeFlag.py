0d4ede6e69fe4f920014779c90199141f44af0db
Uros Petrevski <uros@nodesign.net>
2014-02-27 18:33:25 +0100

    In FW build port corrected for 2 tornados

--- openWrt/firstTimeFlag.py
+++ openWrt/firstTimeFlag.py
@@ -8,16 +8,16 @@ def getConfiguration(path):
     inputFile.close()
     return json.loads(rawData)
 
-
 def saveConfiguration(path, conf):
     inputFile = open(path, 'w')
     print(inputFile)
     ret = inputFile.write(json.dumps(conf, indent=4, sort_keys=True))
     inputFile.close()
-    
+
 config = getConfiguration(sys.argv[1])
 config['first_time_run'] = "YES"
-config['port'] = 80
+config['port'] = 8080
+config['userAppPort'] = 80
 config['debug_mode'] = "False"
 config['extern_projects_path'] = "/weioUser/"
 saveConfiguration(sys.argv[1], config)
