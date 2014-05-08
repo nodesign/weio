d8581555372140cd616f702d38dcdc3279db3574
Uros Petrevski <uros@nodesign.net>
2014-05-08 08:46:53 +0200

    Uper reset testing program improved

--- sandbox/uperReset/adcWeio.py
+++ sandbox/uperReset/adcWeio.py
@@ -1,9 +1,11 @@
 from weioLib.weioIO import *
+from weioLib import weioIO
 from weioLib.weioGpio import WeioGpio
 
 
 for i in range(0,3):
-    w = initWeio(WeioGpio())
+    w = WeioGpio()
+    weioIO.gpio = w
     for a in range(0,10):
         digitalWrite(19,1)
         val = analogRead(24)
@@ -11,6 +13,7 @@ for i in range(0,3):
         delay(90)
         digitalWrite(19,0)
         delay(90)
+    w.stopReader()
     w.reset()
 
 
