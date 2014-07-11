41c4d989434403fcc0ddf99f7f4e5ef0fe734b39
Uros Petrevski <uros@nodesign.net>
2014-07-11 23:11:28 +0200

    Board.js small changes

--- www/libs/weio/board.js
+++ www/libs/weio/board.js
@@ -4,12 +4,12 @@
  * Copyright (C) 2013 Nodesign.net, Uros PETREVSKI, Drasko DRASKOVIC
  * All rights reserved
  *
- *               ##      ## ######## ####  #######  
- *               ##  ##  ## ##        ##  ##     ## 
- *               ##  ##  ## ##        ##  ##     ## 
- *               ##  ##  ## ######    ##  ##     ## 
- *               ##  ##  ## ##        ##  ##     ## 
- *               ##  ##  ## ##        ##  ##     ## 
+ *               ##      ## ######## ####  #######
+ *               ##  ##  ## ##        ##  ##     ##
+ *               ##  ##  ## ##        ##  ##     ##
+ *               ##  ##  ## ######    ##  ##     ##
+ *               ##  ##  ## ##        ##  ##     ##
+ *               ##  ##  ## ##        ##  ##     ##
  *                ###  ###  ######## ####  #######
  *
  *                    Web Of Things Platform
@@ -26,25 +26,18 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * This file is part of WEIO.
  *
- * Authors : 
+ * Authors :
  * Uros PETREVSKI <uros@nodesign.net>
  * Drasko DRASKOVIC <drasko.draskovic@gmail.com>
- * 
+ *
  *
  **/
 
 var boardSocket;
 var socketOpened = false;
-
-/*
- * When all DOM elements are fully loaded
- */
-//$(document).ready(function () {
-	               
-//});
 
 
 function connectToBoard() {
@@ -66,64 +59,63 @@ function connectToBoard() {
             _addr = a;
         }
         console.log("WebSocket connecting to board on ", _addr);
-        
+
         /*
          * WebSocket openning
          */
-        
+
         //boardSocket = new WebSocket(_addr);
         boardSocket = new SockJS(_addr);
-        
-        
 
-        //////////////////////////////////////////////////////////////// BOARD JS STATS        
-        
+
+
+        //////////////////////////////////////////////////////////////// BOARD JS STATS
+
         /*
          * On opening of wifi web socket ask server to scan wifi networks
          */
         boardSocket.onopen = function() {
             console.log('Board Web socket is opened');
             socketOpened = true;
-            
+
             var rq = { "request": "pinsInfo", "data":"", "callback":"pinsInfo"};
             boardSocket.send(JSON.stringify(rq));
         };
-        
+
         /*
          * Dashboard parser, what we got from server
          */
         boardSocket.onmessage = function(e) {
             //console.log('Received: ' + e.data);
-            
+
             // JSON data is parsed into object
             data = JSON.parse(e.data);
             console.log(data);
-            
+
             // switch
             if ("requested" in data) {
                 // this is instruction that was echoed from server + data as response
-                instruction = data.requested;  
-                if (instruction in callbacksBoard) 
+                instruction = data.requested;
+                if (instruction in callbacksBoard)
                     callbacksBoard[instruction](data);
             } else if ("serverPush" in data) {
                 // this is instruction that was echoed from server + data as response
-                instruction = data.serverPush;  
-                if (instruction in callbacksBoard) 
+                instruction = data.serverPush;
+                if (instruction in callbacksBoard)
                     callbacksBoard[instruction](data);
             }
         };
-        
+
         boardSocket.onclose = function() {
             console.log('Board Web socket is closed');
             socketOpened = false;
         };
-            
+
     } else {
         var rq = { "request": "pinsInfo", "data":"","callback":"pinsInfo"};
         boardSocket.send(JSON.stringify(rq));
     }
 
-    
 }
 
 function boardData(data) {
@@ -132,12 +124,11 @@ function boardData(data) {
     for (var i=0; i<32; i++) {
      if (data.data.data[i] != -1 ) {
          $("#pin"+String(i)).attr("class", "pin_on");
-     } else { 
+     } else {
          $("#pin"+String(i)).attr("class", "pin");
         }
     }
-    
-          
+
 }
 
 //CALLBACKS////////////////////////////////////////////////////////////////////////////////////////////////////////
