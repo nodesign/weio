e9a751aaa333c9719d9adb0f9e89d7f5fb148163
Uros Petrevski <uros@nodesign.net>
2014-07-18 16:47:09 +0200

    Updated examples

--- examples/buttonToLEDWEB/main.py
+++ examples/buttonToLEDWEB/main.py
@@ -1,12 +1,9 @@
 from weioLib.weioIO import *
-from weioLib.weioUserApi import attach, HIGH, LOW, shared
+from weioLib.weioUserApi import attach
 
-import time
 
 def setup() :
-    attach.event("pera", peraEvent)
+    attach.event("webEvent", webEvent)
 
-def peraEvent(data):
-    print "Jednom Pera"
-    #time.sleep(1)
-    print "Uvek Pera"
+def webEvent(data):
+    print "web event"
