d371cc5ec4538e7aced67a1fab7e4332b5891757
Uros Petrevski <uros@nodesign.net>
2014-07-18 16:46:20 +0200

    examples updated

--- examples/ledBlinkyPython/main.py
+++ examples/ledBlinkyPython/main.py
@@ -1,16 +1,2 @@
-from weioLib.weioIO import *
-from weioLib.weioUserApi import attach, HIGH, LOW, shared
-
-import time
-
-def setup() :
-    attach.process(blinky)
-
-def blinky() :
-    print "Hello world!"
-    pause = 100
-    while True:
-        digitalWrite(20, HIGH)
-        delay(pause)
-        digitalWrite(20, LOW)
-        delay(pause)
+digitalWrite(18, HIGH)
+        
