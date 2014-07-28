74885db33f5c079698e55139aa384157af18532e
Uros Petrevski <uros@nodesign.net>
2014-07-28 09:38:04 +0200

    Saving personal configuration file between updates

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 echo "This is WeIO post install procedure"
+# Getting backup configuration file from /tmp and storing back to /weio directory
+cp /tmp/config.weio /weio
 
 #cd /
 #cp /weio/scripts/weio_run.sh .
