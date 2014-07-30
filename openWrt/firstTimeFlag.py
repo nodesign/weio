7930cd494338c43f8b32acd6a65f56c4ee690de4
Uros Petrevski <uros@nodesign.net>
2014-07-30 18:38:09 +0200

    Path correction, flash folder will be visible from the beginning

--- openWrt/firstTimeFlag.py
+++ openWrt/firstTimeFlag.py
@@ -19,6 +19,6 @@ config['first_time_run'] = "YES"
 config['port'] = 8080
 config['userAppPort'] = 80
 config['debug_mode'] = "False"
-config['extern_projects_path'] = "/weioUserProjects"
+config['extern_projects_path_flash'] = "/weioUser"
 config['absolut_root_path']= "/weio"
 saveConfiguration(sys.argv[1], config)
