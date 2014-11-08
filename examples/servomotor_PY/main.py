a131752626aeb1bfa432bd6f6bebeac23695e2a4
Uros Petrevski <uros@nodesign.net>
2014-11-08 22:04:15 +0100

    Servo motor example added

--- examples/servomotor_PY/main.py
+++ examples/servomotor_PY/main.py
@@ -7,14 +7,8 @@
 #servomotor library
 # there are 6 pwm pins on weio (23, 22, 21, 20, 19, 18)
 # servomotor use pwm pin. 
-# initServo(pin) return Servo object and attach the Servo variable to a pwm pin
 # write(angle) servo is a vairable of type Servo
 # angle is the value to write to the servo, from 0 to 180
-
-# other functions :
-# writeMilliseconds(ms)
-# readMilliseconds()
-# read()
 
 from weioLib.weio import *
 from things.servomotor import Servo
@@ -23,14 +17,15 @@ def setup():
     attach.process(myProcess)
     
 def myProcess():
-    s = weioLib(Servo,23)
+    print("Hello servo")
+    s = Servo(23)
     while True :
-        for i in range(180):
+        for i in range(181):
             s.write(i)
             print s.read()
             delay(20)
 
-        for i in range(180):
+        for i in range(181):
             s.write(180-i)
-            print s.readMilliseconds()
+            print s.read()
             delay(20)
