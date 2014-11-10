af02ac4b1a35a29f37b93dde46c4d65237843d41
Uros Petrevski <uros@nodesign.net>
2014-11-10 09:27:04 +0100

    Corrected pwmWrite example

--- examples/pwmWrite_PY/main.py
+++ examples/pwmWrite_PY/main.py
@@ -6,7 +6,8 @@
 
 # syntax = pwmWrite(pin, value) or analogWrite(pin, value)
 # there are 6 pwm pins on weio (23, 22, 21, 20, 19, 18)
-# default value is between 0 and 255
+# default value is between 0 and 100% floating points are
+# permitted for greater precision, PWM has 16bits precision
 # pins 18, 19, 20 are connected on RGB LED on the board
 
 
@@ -18,16 +19,17 @@ def setup():
 def loop():
     while True:
         print "fade in"
-        # count from 0 to 255 
-        for i in range(0,256):
+        # count from 0 to 100 % 
+        for i in range(0,101):
             pwmWrite(18,i)
             pwmWrite(19,i)
             pwmWrite(20,i)
             delay(3)
             
         print "fade out"    
-        for i in range(0,256):
-            pwmWrite(18,255-i)
-            pwmWrite(19,255-i)
-            pwmWrite(20,255-i)
+        for i in range(0,101):
+            pwmWrite(18,100-i)
+            pwmWrite(19,100-i)
+            pwmWrite(20,100-i)
             delay(3)
+
