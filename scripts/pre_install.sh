00f251cd76ded73dcfa4f9dcf09081aa758334a4
Uros Petrevski <uros@nodesign.net>
2014-11-12 12:45:33 +0100

    Preinstall migration script repaired

--- scripts/pre_install.sh
+++ scripts/pre_install.sh
@@ -2,5 +2,4 @@
 echo "This is WeIO pre install procedure"
 # This will backup configuration file to /tmp directory and in post install procedure will copy back to /weio directory
 # If any changements are required for config.weio they have to be done here in this script
-cd /weio
-cp weio.config /tmp
+cp /weio/config.weio /tmp
