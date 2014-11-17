3d64cd606d134eb75d2e509f30ad5280e27dac91
manuIO <manuel.imperiale@gmail.com>
2014-11-18 00:39:47 +0100

    blink example: Adding comment header and program comments

--- examples/blink/main.py
+++ examples/blink/main.py
@@ -1,16 +1,38 @@
+#######################################
+#                                     #
+#      BLINKING AN LED ON WEIO        #
+#                                     # 
+#######################################
+
+# Simple standalone application, no web interface
+# Description: write digital values HIGH and LOW to PINS 18, 19 & 20 (connecteds to RGB LED). Use delay() functions to do blinking the led.
+# WeIO functions: digitalWrite(pin, state) - Available in all PINS
+
 from weioLib.weioIO import *
 from weioLib.weioUserApi import attach
 
+
 def setup() :
+    
+    # Attaches blinky function to infinite loop
     attach.process(blinky)
 
 def blinky() :
+    
     while True:
-        digitalWrite(18, HIGH)
-        digitalWrite(19, HIGH)
-        digitalWrite(20, HIGH)
+        
+        # write HIGH value to digital PINS 18, 19 & 20
+        digitalWrite(18, HIGH) # red led
+        digitalWrite(19, HIGH) # green led
+        digitalWrite(20, HIGH) # blue led
+        
+        # wait 100ms
         delay(100)
-        digitalWrite(18, LOW)
-        digitalWrite(19, LOW)
-        digitalWrite(20, LOW)
+        
+        # write LOW value to digital PINS 18, 19 & 20
+        digitalWrite(18, LOW) # red led
+        digitalWrite(19, LOW) # green led
+        digitalWrite(20, LOW) # blue led
+        
+        # wait 100ms
         delay(100)
