6391b83096e8f8c87cb62043fe26ca465e4c944a
manuIO <manuel.imperiale@gmail.com>
2014-11-25 05:27:23 +0100

    DHT11 example: add comments, clean code

--- examples/DHT11/main.py
+++ examples/DHT11/main.py
@@ -1,21 +1,39 @@
+#######################################
+#                                     #
+#  HOW TO HAVE DHT11 SENSOR ON WEIO   #
+#                                     #
+#######################################
+
+# Description: The DHT11 digital sensor is used to measure the ambient 
+#              temperature and relative humidity. This example shows how 
+#              the dhtRead function can be used to read the sensor.  
+# syntax = dhtRead(pin) - available in all PIN's
+
 from weioLib.weio import *
 
 def setup():
+    # attaches myProcess to infinite loop
     attach.process(myProcess)
     
 def myProcess():
-    import time
-    start = time.time()
+    # define input pin
+    pin = 8
+    # get start time
+    startTime = time.time()
+    # create infinite loop
     while True:
-        a = dhtRead(8)
-        result = a[0] + a[1] + a[2] + a[3]
-        if result == a[4]:
-            print "Measured at : %.0f" %(time.time()-start)
-            print "Humidity = %d.%d"%(a[0], a[1])
-            print "Temperature = %d.%d"%(a[2], a[3])
+        # get data from sensor
+        data = dhtRead(pin)
+        # parse data
+        result = data[0] + data[1] + data[2] + data[3]
+        if result == data[4]:
+            print "Measured at : %.0f" %(time.time()-startTime)
+            print "Humidity = %d.%d"%(data[0], data[1])
+            print "Temperature = %d.%d"%(data[2], data[3])
             print "--------------"
         else:
-            print "Read error at %.0f" %(time.time()-start)
+            print "Read error at %.0f" %(time.time()-startTime)
             print "Packet : ",a
             print "--------------"
+        # wait 5 seconds    
         delay(5000)
