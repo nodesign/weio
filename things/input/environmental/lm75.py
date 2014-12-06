ac5f289c4118bbee17e11d92fb74d1d3af8587b8
Uros Petrevski <uros@nodesign.net>
2014-12-06 11:27:40 +0100

    LM75 driver repaired

--- things/input/environmental/lm75.py
+++ things/input/environmental/lm75.py
@@ -1,39 +1,13 @@
 from struct import unpack
-from IoTPy.pyuper.utils import IoTPy_ThingError
+from weioLib.weio import *
 
 
 class Lm75:
-    """
-    Lm75 temperature sensor class.
-
-    :param interface:  I2C communication interface.
-    :type interface: :class:`IoTPy.pyuper.i2c.I2C`
-    :param sensor_address: Lm75 sensor I2C address. Optional, default 0x48 (72).
-    :type sensor_address: int
-    """
-
-    def __init__(self, interface, address=0x48):
-        self.interface = interface
+    def __init__(self, address=0x48):
         self.address = address
-
-    def __enter__(self):
-        return self
-
-    def temperature(self):
-        """
-        Read and return temperature.
-
-        :return: Temperature in celsius.
-        :rtype: int
-        :raise: IoTPy_ThingError
-        """
-        try:
-            result_raw = self.interface.transaction(self.address, '\x00', 2)
-            result_integer = unpack('>H', result_raw)[0]
-            temperature = result_integer / 256.0
-        except IoTPy_ThingError:
-            raise IoTPy_ThingError("LM75 - temperature reading error.")
-        return temperature
-
-    def __exit__(self, ex_type, ex_value, traceback):
-        pass
+        self.i2c = initI2C()
+    
+    def getTemperature(self):
+        val = self.i2c.transaction(self.address, "\x00", 2)
+        temp = unpack('>H', val[0])[0]
+        return temp/256.0
