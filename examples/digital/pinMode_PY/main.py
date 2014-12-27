a6e13c0f0bf8a74211ab05b02eae9b55da3e6669
manuIO <manuel.imperiale@gmail.com>
2014-12-27 19:51:54 +0100

    pinMode_PY example improved

--- examples/digital/pinMode_PY/main.py
+++ examples/digital/pinMode_PY/main.py
@@ -4,27 +4,35 @@
 #                                     #
 #######################################
 
-# Descriptions: This example shows how to set input mode for 
-#               digitalRead purpose. 
+# Descriptions: This example shows how to set input mode for digitalRead
+#               purpose. You can configure pin as a regular input or    
+#               enable the internal pullup and pulldown resistors.
 #
-# Syntax: pinMode(pin, mode) or portMode()
-# Available modes are : INPUT, PULL_DOWN, PULL_UP
+# Syntax: pinMode(pin, mode) or portMode(pin, mode)
+#
+# pin : all pins availables
+# mode : INPUT, PULL_DOWN, PULL_UP
 
 from weioLib.weio import *
 
 def setup() :
-    attach.process(loop)
+    attach.process(buttonLoop)
     
-def loop() :
-    # define pin for reading
-    pin = 25
+def buttonLoop() :
+    # define pins for reading
+    pinDown = 24
+    pinUp = 25
     # set pin 25 to pull_up mode
-    pinMode(pin,PULL_UP)
+    pinMode(pinDown,PULL_DOWN)
+    pinMode(pinUp,PULL_UP)
     # create infinite loop
     while True:
         # read digital value on pin 25
-        val =  digitalRead(pin)
+        valDown =  digitalRead(pinDown)
+        valUp =  digitalRead(pinUp)
         # print result
-        print (val)
-        # wait 200msw
-        delay(200)
+        print "Pin",pinDown,"state :",valDown
+        print "Pin",pinUp,"state :",valUp
+        print ""
+        # wait 500ms
+        delay(500)
