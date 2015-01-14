f1af2b0eeb4b3f2105925e31a70c6fc5f97d2dac
Uros Petrevski <uros@nodesign.net>
2015-01-14 10:08:05 +0100

    Scan wifi example

---
+++ examples/wifi/wifiScan/main.py
@@ -0,0 +1,28 @@
+#######################################
+#                                     #
+#   Scan wifi cells around WeIO and   #
+#   get basic info                    #
+#                                     #
+#######################################
+from weioLib.weio import *
+from weioWifi import iwInfo
+
+def setup():
+    attach.process(myProcess)
+    
+def myProcess():
+    
+    print("wifi scan")
+    wifi = iwInfo.IWInfo("wlan0")
+
+    wifi.refresh()
+    cells = wifi.parseRawData(wifi.rawdata)
+    print "Number of detected Wifi cells =", len(cells)
+    
+    # list detected cells
+    for n in cells:
+        cell = cells[n]
+        print cell["ESSID"], cell["Quality"]
+        # possible parameters :
+        # Number, MAC, ESSID, Protocol, Mode, Frequecy, Channel, Signal, Quality,
+        # Encryption, Bit Rates, Extra
