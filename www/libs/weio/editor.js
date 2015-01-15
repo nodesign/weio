e30209edd0e0b2d9429a4daf69936370947c22dd
Nikola <nikola.marcetic@ntsystems.rs>
2015-01-15 18:34:05 +0100

    -- Header BSD licence added.

--- www/libs/weio/editor.js
+++ www/libs/weio/editor.js
@@ -1,43 +1,51 @@
 /**
- *
- * WEIO Web Of Things Platform
- * Copyright (C) 2013 Nodesign.net, Uros PETREVSKI, Drasko DRASKOVIC
- * All rights reserved
- *
- *               ##      ## ######## ####  #######
- *               ##  ##  ## ##        ##  ##     ##
- *               ##  ##  ## ##        ##  ##     ##
- *               ##  ##  ## ######    ##  ##     ##
- *               ##  ##  ## ##        ##  ##     ##
- *               ##  ##  ## ##        ##  ##     ##
- *                ###  ###  ######## ####  #######
- *
- *                    Web Of Things Platform
- *
- * WEIO is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * WEIO is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * This file is part of WEIO.
- *
- * Authors :
- * Uros PETREVSKI <uros@nodesign.net>
- * Drasko DRASKOVIC <drasko.draskovic@gmail.com>
- *
- * Special thanks to :
- * Marko Sutija <markosutija@gmail.com> - great help in coding frontend
- *
- **/
-
+*
+* WEIO Web Of Things Platform
+* Copyright (C) 2013 Nodesign.net, Uros PETREVSKI, Drasko DRASKOVIC
+* All rights reserved
+*
+*               ##      ## ######## ####  #######  
+*               ##  ##  ## ##        ##  ##     ## 
+*               ##  ##  ## ##        ##  ##     ## 
+*               ##  ##  ## ######    ##  ##     ## 
+*               ##  ##  ## ##        ##  ##     ## 
+*               ##  ##  ## ##        ##  ##     ## 
+*                ###  ###  ######## ####  #######
+*
+*                    Web Of Things Platform
+*
+* This file is part of WEIO and is published under BSD license.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+* 1. Redistributions of source code must retain the above copyright
+*    notice, this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in the
+*    documentation and/or other materials provided with the distribution.
+* 3. All advertising materials mentioning features or use of this software
+*    must display the following acknowledgement:
+*    This product includes software developed by the WeIO project.
+* 4. Neither the name WeIO nor the
+*    names of its contributors may be used to endorse or promote products
+*    derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY WEIO PROJECT AUTHORS AND CONTRIBUTORS ''AS IS'' AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL WEIO PROJECT AUTHORS AND CONTRIBUTORS BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+* Authors : 
+* Uros PETREVSKI <uros@nodesign.net>
+* Drasko DRASKOVIC <drasko.draskovic@gmail.com>
+*
+**/
 
 /*
  * SockJS object, Web socket
