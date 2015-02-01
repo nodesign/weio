34b85cb4171ff04759007223f0cfd673df27440f
Drasko DRASKOVIC <drasko.draskovic@gmail.com>
2015-02-01 22:06:59 +0100

    Added video packages in OpenWrt

--- openWrt/pkg_install.sh
+++ openWrt/pkg_install.sh
@@ -10,7 +10,6 @@
 ./scripts/feeds install python
 ./scripts/feeds install avahi-daemon
 ./scripts/feeds install ntpd
-./scripts/feeds install motion
 ./scripts/feeds install mjpg-streamer
 ./scripts/feeds install fswebcam
 ./scripts/feeds install libv4l
