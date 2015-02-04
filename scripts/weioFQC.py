e936ee615aa18353af1f236d62450b6ac72b3288
Paul RATHGEB <paul.rathgeb@skynet.be>
2015-02-04 11:40:23 +0100

    Refactored the FQC script Instead of detecting the bootloader, which needs to parse all block devices and takes a lot of time, this version use SFP to detect if the firmware is compatible with WeIO. The execution time has been divided by 10, from 7.45s to 0.8s. Now, the script is more stable. No side effects have been noticed anymore. In addition, the START and STOP levels have been optimized in the init.d script.

--- scripts/weioFQC.py
+++ scripts/weioFQC.py
@@ -1,76 +1,139 @@
-#!/usr/bin/python -u
-import sys, os, glob, logging, platform, json, signal, datetime
-sys.path.append(os.getcwd())
-import time
+#!/usr/bin/env python
+# encoding: utf-8
 
-# This class detect if the LPC is shown on the USB bus
-class Detector:
-    def __init__(self, reset_pin=17, program_pin=22):
-        reset_pin_str = str(reset_pin)
-        program_pin_str = str(program_pin)
-        self._gpio(reset_pin_str, "/sys/class/gpio/export")
-        self._gpio(program_pin_str, "/sys/class/gpio/export")
-        self._gpio("out","/sys/class/gpio/gpio"+reset_pin_str+"/direction")
-        self._gpio("out","/sys/class/gpio/gpio"+program_pin_str+"/direction")
-        self.uper_reset = "/sys/class/gpio/gpio"+reset_pin_str+"/value"
-        self.uper_program = "/sys/class/gpio/gpio"+program_pin_str+"/value"
+import struct
+import types
+import glob
 
-    def _gpio(self, content, file_name):
-        try:
-            file_id = os.open(file_name, os.O_WRONLY)
-            os.write(file_id, content)
-            os.close(file_id)
-        except OSError:
-            pass
+import serial
 
-    def _reset_uper(self):
-        self._gpio("1",self.uper_reset)
-        self._gpio("0",self.uper_reset)
+class detectFW:
+    def __init__(self):
+        pass
 
-    def detect(self):
-        uper_flash_pattern = "CRP DISABLD"
-
-        # put UPER in to programming mode
-        self._gpio("1",self.uper_reset)
-        self._gpio("1",self.uper_program)
-        self._gpio("0",self.uper_reset)
-        time.sleep(2) # wait for linux to settle after UPER reboot in to pgm state
-        self._gpio("0",self.uper_program)
-
-        # find UPER block device
-        list_block_devs = glob.glob("/sys/block/sd*")
-        block_device_name = ''
-        header = ''
-        for try_device_name in list_block_devs:
-            try_device_name = "/dev/" + try_device_name.split('/')[-1]
+    def detector(self):
+        ser = None
+        ports_list = glob.glob("/dev/ttyACM*")
+        for my_port in ports_list:
             try:
-                block_device = os.open(try_device_name, os.O_RDWR)
-                os.lseek(block_device, 3 * 512, os.SEEK_SET)
-                header = os.read(block_device,11)
-                time.sleep(0.35) # reading can be slowww
-                os.close(block_device)
-            except OSError:
-                pass
-            if header == uper_flash_pattern: # "CRP DISABLD"
-                block_device_name = try_device_name # found UPER
-                break;
-        if block_device_name == '':
-            return 0
+                port_to_try = serial.Serial(
+                    port=my_port,
+                    baudrate=230400, #virtual com port on USB is always max speed
+                    parity=serial.PARITY_ODD,
+                    stopbits=serial.STOPBITS_ONE,
+                    bytesize=serial.EIGHTBITS,
+                    timeout=0.1
+                )
+                port_to_try.write(self.encode_sfp(255, []))
+                uper_response = port_to_try.read(1)    #read one, blocking
+                n = port_to_try.inWaiting()        #look if there is more
+                if n:
+                    uper_response = uper_response + port_to_try.read(n)
+                    if self.decode_sfp(uper_response)[0] == -1: # found port with UPER
+                        ser = port_to_try
+                        break
+                port_to_try.close()
+            except:
+                print "Exception"
+                return False
+        if not ser:
+            print "Not found"
+            return False
 
-        # reset UPER
-        self._reset_uper()
-        time.sleep(.2)
-        return 1
-        
+        return True
+
+    def _encode_int(self, intarg):
+        if intarg < 64:
+            return chr(intarg)
+        packedint = struct.pack('>I', intarg).lstrip('\x00')
+        return chr(0xc0 | (len(packedint) - 1)) + packedint
+
+    def _encode_bytes(self, bytestr):
+        if len(bytestr) < 64:
+            return chr(0x40 | len(bytestr)) + bytestr
+        packedlen = struct.pack('>I', len(bytestr)).lstrip('\x00')
+        if len(packedlen) == 1:
+            return '\xc4' + packedlen + bytestr
+        elif len(packedlen) == 2:
+            return '\xc5' + packedlen + bytestr
+
+    def encode_sfp(self, command, args):
+        """
+        Construct binary SFP command.
+
+        :param command: SFP command ID.
+        :type command: int
+        :param args: A list of SFP arguments, which can be either an integer or a byte collection (string).
+        :type args: list
+        :return: Binary SFP command.
+        :rtype: str
+        """
+        functions = {
+            types.StringType: self._encode_bytes,
+            bytearray: self._encode_bytes,
+            types.IntType: self._encode_int
+        }
+        sfp_command = chr(command) + ''.join(str(functions[type(arg)](arg)) for arg in args)
+        sfp_command = '\xd4' + struct.pack('>H', len(sfp_command)) + sfp_command
+        return sfp_command
+
+    def decode_sfp(self, buffer):
+        """
+        Decode SFP command from byte buffer.
+
+        :param buffer: A byte buffer which stores SFP command.
+        :type buffer: str
+        :return: A list containing decoded SFP function ID and arguments (if any).
+        """
+        result = []
+        if buffer[0:1] != '\xd4':
+            return result
+        buflen = struct.unpack('>H', buffer[1:3])[0] + 3
+        result.append(struct.unpack('b', buffer[3:4])[0])
+        pointer = 4
+        args = []
+        while pointer < buflen:
+            argtype = ord(buffer[pointer:pointer + 1])
+            pointer += 1
+            if argtype < 64:                    # short int
+                args.append(argtype)
+            elif argtype < 128:                    # short str
+                arglen = argtype & 0x3f
+                args.append(buffer[pointer:pointer + arglen])
+                pointer += arglen
+            else:
+                arglen = argtype & 0x0f
+                if arglen < 4:            # decoding integers
+                    if arglen == 0:
+                        args.append(ord(buffer[pointer:pointer + 1]))
+                    elif arglen == 1:
+                        args.append(struct.unpack('>H', buffer[pointer:pointer + 2])[0])
+                    elif arglen == 2:
+                        args.append(struct.unpack('>I', '\x00' + buffer[pointer:pointer + 3])[0])
+                    elif arglen == 3:
+                        args.append(struct.unpack('>I', buffer[pointer:pointer + 4])[0])
+                    pointer += arglen + 1
+                else:
+                    if argtype == 0xc4:        # decoding strings
+                        arglen = ord(buffer[pointer:pointer + 1])
+                    elif argtype == 0xc5:
+                        arglen = struct.unpack('>H', buffer[pointer:pointer + 2])[0]
+                        pointer += 1
+                    pointer += 1
+                    args.append(buffer[pointer:pointer + arglen])
+                    pointer += arglen
+        result.append(args)
+        return result
+
 if __name__ == "__main__":
     ### Detect the LPC
-    detect = Detector()
-    res = detect.detect()
-    time.sleep(3)
+    fw = detectFW()
+    res = fw.detector()
 
     if not res:
         print "LPC not found !"
         import ledBlink as led
         while True:
             led.blink(.1)
-
+    else:
+        print "LPC found with a functionnal firmware"
