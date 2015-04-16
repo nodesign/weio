3c5f84b4305b02f5a71ccb7c04be537cc865589c
manuIO <manuel.imperiale@gmail.com>
2015-04-16 21:49:05 +0200

    interrupt_PY improved

--- examples/digital/interrupt_PY/main.py
+++ examples/digital/interrupt_PY/main.py
@@ -1,42 +1,42 @@
 #######################################
 #                                     #
-#   Example of user-defined interrupt #
-#   on WEIO                           #
+#          HOW TO ATTACH AN           #
+#         INTERRUPT ON WEIO           #
 #                                     #
 #######################################
 
+# Descriptions: This example shows how to attach an interrupt on pin 18.
+#               Callback that will be called on interrupt retuns an
+#               object with interrupt type and pin number, this is
+#               useful to trace pin on which interrupt has been rised.
+#
+# syntax = attachInterrupt(pin, type, callback, object)
+#
+# Available pins: All pins
+# Available modes: RISING, FALLING, HIGH, LOW, CHANGE
+    
 from weioLib.weio import *
 
 def setup():
     attach.process(myProcess)
     
-    pin = 5
-
-    # attach user-defined interrupt handler to pin 5
-    # in attachInterrupt parameters are :
-    # pin for interrupt,
-    # mode can be RISING, FALLING, CHANGE
-    # callback that will be called on interrupt
-    # some object that will be returned in callback, this is useful to trace
-    # pin on which interrupt has been rised
+def myProcess():
+    # attach user-defined interrupt handler to pin 18
+    pin = 18
     attachInterrupt(pin, RISING, testInt, pin)
     
-def myProcess():
     # Make one infinite loop with blinking LED
     while True:
         digitalWrite(18,LOW)
         delay(200)
         digitalWrite(18,HIGH)
         delay(200)
-        
     
 # This is interrupt callback
 def testInt(event,obj):
     print "*** INTERRUPT ***"
-    # event dictionary and function that brings in humain readable format 
+    # event dictionary and function that brings in humain readable format
     # interrupt mode
     # Passed object contains pin number of inetrrupt
     eventType = getInterruptType(event["type"])
-    print eventType, "on pin", obj 
-    
-    
+    print eventType, "on pin", obj
