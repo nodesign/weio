5bc76bc4febe1f5f1dd0b1c5066c6f7fba2b6553
manuIO <manuel.imperiale@gmail.com>
2015-04-18 14:02:09 +0200

    Changing phrase warming up the engines to weio is running

--- weioPlayer.py
+++ weioPlayer.py
@@ -132,7 +132,7 @@ class WeioPlayer():
         # Inform client the we run subprocess
         data = {}
         data['requested'] = rq['request']
-        data['status'] = "Warming up the engines..."
+        data['status'] = "WeIO is running!"
         self.send(json.dumps(data))
 
         consoleWelcome = {}
