10a459094a08facbafff1e9dfd02cc8c4bba28f1
DervanM <dervanm@gmail.com>
2016-03-28 21:10:44 +0200

    Handler gyrosAngles & set color to background and Led separetly

--- examples/webApps/bgColorLEDWEB/main.py
+++ examples/webApps/bgColorLEDWEB/main.py
@@ -4,50 +4,67 @@ import colorsys
 import time
  
 def setup():
-    # Attaches interrupt from Web client to "message" string
-    attach.event('message', buttonHandler)
+    # Attaches attaches event/genericMessage "gyro" to function gyroHandler
+    attach.event('gyro', gyroHandler)
     sharedVar[0] = 0
  
-def buttonHandler(dataIn) :
-   if dataIn is not None :
+def gyroHandler(dataIn) :
+   if dataIn[0] is not None :
        #print dataIn
        #print "FROM BROWSER: ", dataIn["data"], " uuid: ", dataIn["uuid"]
-       beta = float(dataIn)
+
+       # gyroscope angles
+       alpha = dataIn[0]
+       beta  = dataIn[1]
+       gamma = dataIn[2]
+
+       # print gyroscope angles on the console
+       #print "Gyroscope angles => alpha = ",alpha,"beta = ",beta,"gamma = ",gamma
+
+       #We normalize Intervalle to [0,1]
+       # alpha Intervalle [0,360] to [0,1]
+       alphaI = proportion(alpha,0.0,360.0, 0.0,1.0)
+
+       # beta Intervalle [-180,180] to [0,1]
+       beta = beta + 180
+       betaI = proportion(beta,0.0,360.0, 0.0,1.0)
+
+       #gamma Intervalle [-90,90] to [0,1]
+       gamma = gamma +90
+       gammaI = proportion(gamma,0.0,180.0, 0.0,1.0)
+
+       #print " Intervale => alphaI = ",alphaI,"betaI =",betaI,"gammaI = ",gammaI
+
+       #rgb output is 0.0-1.0 transform to 0-255
+       red = int(alphaI*255.0)
+       green = int(betaI*255.0)
+       blue = int(gammaI*255.0)
  
-       # put limiters
-       if (beta>88):
-           beta = 88
-       if (beta<-88):
-           beta = -88
- 
-       # map interval between -88 and 88 to 0.0 - 1.0
-       hue = proportion(beta,-88.0,88.0, 0.0,1.0)
-       # drive HUE color and transform to rgb for LED
-       rgb = colorsys.hsv_to_rgb(hue, 1.0, 1.0)
- 
-       # rgb output is 0.0-1.0 transform to 0-255
-       red = int(rgb[0]*100.0)
-       green = int(rgb[1]*100.0)
-       blue = int(rgb[2]*100.0)
- 
-       print red, " ", green, " ", blue
+       #print red, " ", green, " ", blue
        # Export color to LED
        setColor(red,green,blue)
- 
-def setColor(r,g,b):
+
+       # rgb output is 0.0-1.0 transform to 0-100 LED rgb
+       redLed = int(alphaI*100.0)
+       greenLed = int(betaI*100.0)
+       blueLed = int(gammaI*100.0)
+       setColorLed(redLed,greenLed,blueLed)
+
+def setColorLed(r,g,b):
     # LED values are comprised between 0 and 100(%).
     # For hardware reasons, LEDs lit when pin is LOW (0V)
     # This is why the value is inverted here : 100% - led value
     pwmWrite(18,100-r)
     pwmWrite(19,100-g)
     pwmWrite(20,100-b)
- 
+
+def setColor(r,g,b):
     colorData = {}
     colorData["red"] = r
     colorData["green"] = g
     colorData["blue"] = b
- 
+
     serverPush("usrMsg", colorData)
- 
+
 def proportion(value,istart,istop,ostart,ostop) :
        return float(ostart) + (float(ostop) - float(ostart)) * ((float(value) - float(istart)) / (float(istop) - float(istart)))
